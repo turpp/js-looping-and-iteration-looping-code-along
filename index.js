67529de81cf1afd808ab4fcbce0a378626bac917
turpp <white.tripp@icloud.com>
2021-01-04 18:25:27 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array, eventName){
+    let newArray=[]
+    for (let i=0; i<array.length; i++){
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return newArray
+}
+
+function countDown(number){
+    var i= number
+    while (i != -1) {
+        console.log(i);
+        i--;
+    }
+}
